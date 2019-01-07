b747a6d9b1321635982117b3712d92462c9fcd7c
swoldetsadick <kebrab67@gmail.com>
2019-01-07 22:36:07 +0100

    adding d3.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+
   title = 'samuels-app';
+
+  ngAfterViewInit() {
+    d3.select('p').style('background-color', 'yellow');
+  }
+
 }
